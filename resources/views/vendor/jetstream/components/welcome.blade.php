baf1f24b013402e7caee1928bdcff1fddee7f4df
Risda Tamam <risda123tamam@gmail.com>
2021-07-25 03:52:58 +0700

    Push

---
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -0,0 +1,37 @@
+<div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+    <div>
+        <x-jet-application-logo class="block h-12 w-auto" />
+    </div>
+
+    <div class="mt-8 text-2xl">
+        Welcome {{ Auth::user()->name }}
+    </div>
+
+    <div class="mt-6 text-gray-500">
+        Tugas Pemrograman Web pertemuan 14
+    </div>
+</div>
+
+<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2">
+    <div class="p-6">
+        <div class="flex items-center">
+            <div class="ml-4 text-lg text-gray-600 leading-7 font-semibold"><a href="#">User</a></div>
+        </div>
+
+        <div class="ml-12">
+            <div class="mt-2 text-sm text-gray-500">
+                Halaman CRUD User
+            </div>
+
+            <a href="#">
+                <div class="mt-3 flex items-center text-sm font-semibold text-indigo-700">
+                        <div>Table User</div>
+
+                        <div class="ml-1 text-indigo-500">
+                            <svg viewBox="0 0 20 20" fill="currentColor" class="w-4 h-4"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
